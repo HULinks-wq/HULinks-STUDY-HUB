45395500a12a649f23562d852fa86ab693e09e00
HULinks-wq <huloniks05@gmail.com>
2026-03-31 11:15:09 +0200

    index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 
 import health from "./health";
-import ai from "./ai";
+import { ai } from "./ai";
 import uploads from "./uploads";
 
 export function registerRoutes(app: Express) {
