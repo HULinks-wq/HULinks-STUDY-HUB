930d4a27e1aa37b1f0d592d09911d5a0509092e0
Thulani Mama <huloniks05@gmail.com>
2026-04-03 18:21:31 +0200

    fix binding properly

--- server/index.ts
+++ server/index.ts
@@ -4,11 +4,10 @@ import routes from "./routes";
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
