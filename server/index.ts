b2b49671ebece24731e76e0c57d4ad1d2f03016e
Thulani Mama <huloniks05@gmail.com>
2026-04-04 11:15:23 +0200

    fix cors properly

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,10 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", // allow all (for now)
+}));
+
 app.use(express.json());
 
 app.use(routes);
